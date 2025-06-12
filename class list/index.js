20de478145741a952f4638ea88b2bc8feb8ca089
manishgopali <manishgopali123@gmail.com>
2025-06-12 12:58:14 +0545

    update

---
+++ class list/index.js
@@ -0,0 +1,11 @@
+//classList = element property in javascript used to interact
+//with an element's list of classes(CSS classes)
+// allows you to make reuseable classes for many elements
+//across your webpage
+
+
+//add()
+//remove()
+//toggle(Remove if present , Add if not)
+//replace (oldClass,newClass)
+//contains()
